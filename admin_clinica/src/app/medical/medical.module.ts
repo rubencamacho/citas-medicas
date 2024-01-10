08b9edc73b9d273cda136fed4be35c5d399780eb
Ruben Camacho <rubencamachorguez@gmail.com>
2024-01-10 12:23:58 +0000

    Acceso y configuración de permisos. Middleware y Policies

---
+++ admin_clinica/src/app/medical/medical.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MedicalRoutingModule } from './medical-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MedicalRoutingModule
+  ]
+})
+export class MedicalModule { }
--- api-clinica/routes/api.php
+++ api-clinica/routes/api.php
@@ -20,14 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group([
- 
+
     // 'middleware' => 'auth:api',
-    'prefix' => 'auth'
- 
+    'prefix' => 'auth',
+    // 'middleware' => ['role:admin','permission:publish articles'],
+
 ], function ($router) {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
+    Route::post('/list', [AuthController::class, 'list']);
+    Route::post('/reg', [AuthController::class, 'reg']);
 });
